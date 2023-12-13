660c83474b3522b2f51bb547d7ddb3ac6b450d2c
BlackHawk <vergil31@hotmail.fr>
2023-12-13 09:56:04 +0100

    Correction README, modification create_account

--- cypress/e2e/create_account.cy.js
+++ cypress/e2e/create_account.cy.js
@@ -1,5 +1,8 @@
 describe('Create account', () => {
   const baseurl = "http://localhost:8080/"
+  const name = 'John'
+  const email = 'john-azerty@mail.fr'
+  const password = 'Azerty-12'
   it('Nominal test', () => {
     cy.visit('/')
     
