bc4ba814c7ba671015f32ac7117288374866abec
BlackHawk <vergil31@hotmail.fr>
2023-12-13 09:42:12 +0100

    Correction README

--- README.md
+++ README.md
@@ -8,4 +8,5 @@
 
 ## How to test
     npm init
+    npm install
     npx cypress open
--- cypress/e2e/create_account.cy.js
+++ cypress/e2e/create_account.cy.js
@@ -2,5 +2,6 @@ describe('Create account', () => {
   const baseurl = "http://localhost:8080/"
   it('Nominal test', () => {
     cy.visit('/')
+    
   })
 })
